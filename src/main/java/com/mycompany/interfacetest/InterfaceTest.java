1d0d76a4b18191b2f671ce7cdf7aebe9ef1e2f1f
Avery Manzon <62368112+AVERYMNZN@users.noreply.github.com>
2024-04-24 16:52:59 +0800

    Added separate methods for login database connection and student database connection

--- src/main/java/com/mycompany/interfacetest/InterfaceTest.java
+++ src/main/java/com/mycompany/interfacetest/InterfaceTest.java
@@ -6,17 +6,29 @@ package com.mycompany.interfacetest;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
 
 public class InterfaceTest {
 
-    public static Connection conn() {
+    public static Connection loginConn() {
         try {
             String url = "jdbc:ucanaccess://C://database//EduSync11.accdb";
             Connection conn = DriverManager.getConnection(url);
             return conn;
         } catch (Exception e) {
-            System.out.println(e);
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+    
+    public static Connection studentConn() {
+        try {
+            String url = "jdbc:ucanaccess://C://database//AttendanceSystem.accdb";
+            Connection conn = DriverManager.getConnection(url);
+            return conn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
         }
         return null;
     }

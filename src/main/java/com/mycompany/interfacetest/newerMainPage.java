5312773f268bc07675a50f335514b3ce0a18d78f
Avery Manzon <62368112+AVERYMNZN@users.noreply.github.com>
2024-04-28 05:56:32 +0800

    Modified main page

--- src/main/java/com/mycompany/interfacetest/newerMainPage.java
+++ src/main/java/com/mycompany/interfacetest/newerMainPage.java
@@ -15,14 +15,17 @@ import java.awt.Font;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.RowFilter;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 public class newerMainPage extends javax.swing.JFrame {
     
@@ -64,10 +67,12 @@ public class newerMainPage extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         studentsPanel = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        studentsTable = new javax.swing.JTable();
+        studentsCardStudentsTable = new javax.swing.JTable();
         fetchStudentsDataButton = new javax.swing.JButton();
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        studentsCardStudentsSearchField = new javax.swing.JTextField();
+        studentsCardSearchButton = new javax.swing.JButton();
+        studentsCardSectionsCombobox = new javax.swing.JComboBox<>();
+        fetchNewDataButton = new javax.swing.JButton();
 
         jScrollPane1.setViewportView(jTextPane1);
 
@@ -165,7 +170,7 @@ public class newerMainPage extends javax.swing.JFrame {
 
         cardPanel.add(dashboardPanel, "dashboardCard");
 
-        studentsTable.setModel(new javax.swing.table.DefaultTableModel(
+        studentsCardStudentsTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -173,8 +178,8 @@ public class newerMainPage extends javax.swing.JFrame {
                 "First Name", "Last Name", "Student Number", "Grade Level", "Strand"
             }
         ));
-        jScrollPane2.setViewportView(studentsTable);
-        studentsTable.getAccessibleContext().setAccessibleName("");
+        jScrollPane2.setViewportView(studentsCardStudentsTable);
+        studentsCardStudentsTable.getAccessibleContext().setAccessibleName("");
 
         fetchStudentsDataButton.setText("Fetch Data");
         fetchStudentsDataButton.addActionListener(new java.awt.event.ActionListener() {
@@ -183,55 +188,61 @@ public class newerMainPage extends javax.swing.JFrame {
             }
         });
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        studentsCardSearchButton.setText("Search");
+        studentsCardSearchButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                studentsCardSearchButtonActionPerformed(evt);
+            }
+        });
 
-        jLabel1.setText("Filter");
+        studentsCardSectionsCombobox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Select Section", "ICT 12 A", "ICT 12 B", "ICT 12 C", "ICT 12 D", " " }));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(332, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(194, Short.MAX_VALUE))
-        );
+        fetchNewDataButton.setText("Fetch New");
+        fetchNewDataButton.setToolTipText("");
+        fetchNewDataButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                fetchNewDataButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout studentsPanelLayout = new javax.swing.GroupLayout(studentsPanel);
         studentsPanel.setLayout(studentsPanelLayout);
         studentsPanelLayout.setHorizontalGroup(
             studentsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, studentsPanelLayout.createSequentialGroup()
+                .addContainerGap(163, Short.MAX_VALUE)
+                .addGroup(studentsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(fetchStudentsDataButton, javax.swing.GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE)
+                    .addComponent(fetchNewDataButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(161, 161, 161)
                 .addGroup(studentsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 585, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(studentsPanelLayout.createSequentialGroup()
-                        .addGap(138, 138, 138)
-                        .addComponent(fetchStudentsDataButton, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(studentsPanelLayout.createSequentialGroup()
-                        .addGap(25, 25, 25)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 585, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(studentsCardSectionsCombobox, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(studentsCardStudentsSearchField, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(studentsCardSearchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(15, 15, 15))
         );
         studentsPanelLayout.setVerticalGroup(
             studentsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(studentsPanelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 708, Short.MAX_VALUE)
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, studentsPanelLayout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(160, 160, 160)
-                .addComponent(fetchStudentsDataButton, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(232, 232, 232))
+                .addGroup(studentsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(studentsCardStudentsSearchField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(studentsCardSearchButton)
+                    .addComponent(studentsCardSectionsCombobox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(studentsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(studentsPanelLayout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 676, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(studentsPanelLayout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addComponent(fetchStudentsDataButton, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(fetchNewDataButton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
         );
 
         cardPanel.add(studentsPanel, "studentsCard");
@@ -276,7 +287,7 @@ public class newerMainPage extends javax.swing.JFrame {
             st = studentConn.createStatement();
             rs = st.executeQuery(sqlQuery);
 
-            DefaultTableModel studentsTableModel = (DefaultTableModel)studentsTable.getModel();
+            DefaultTableModel studentsTableModel = (DefaultTableModel)studentsCardStudentsTable.getModel();
             studentsTableModel.setRowCount(0);
 
             while (rs.next()) {
@@ -325,6 +336,132 @@ public class newerMainPage extends javax.swing.JFrame {
         mainpageCardLayout.show(cardPanel, "studentsCard");
     }//GEN-LAST:event_studentsButtonLabelActionPerformed
 
+    private void studentsCardSearchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_studentsCardSearchButtonActionPerformed
+        // TODO add your handling code here:
+        DefaultTableModel ob = (DefaultTableModel) studentsCardStudentsTable.getModel();
+        TableRowSorter<DefaultTableModel> obj = new TableRowSorter<>(ob);
+        studentsCardStudentsTable.setRowSorter(obj);
+        obj.setRowFilter(RowFilter.regexFilter(studentsCardStudentsSearchField.getText()));
+    }//GEN-LAST:event_studentsCardSearchButtonActionPerformed
+
+    private void fetchNewDataButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fetchNewDataButtonActionPerformed
+        // TODO add your handling code here:
+         String selectedSection = (String) studentsCardSectionsCombobox.getSelectedItem();
+        
+        if("ICT 12 A".equals(selectedSection)) {
+            System.out.println("Hello A");
+            try {
+                String sqlQuery = "SELECT * FROM ICT_12A";
+                st = studentConn.createStatement();
+                rs = st.executeQuery(sqlQuery);
+
+                DefaultTableModel studentsTableModel = (DefaultTableModel)studentsCardStudentsTable.getModel();
+                studentsTableModel.setRowCount(0);
+
+                while (rs.next()) {
+                    String studentFirstName = String.valueOf(rs.getString("Student_First_Name"));
+                    String studentLastName = String.valueOf(rs.getString("Student_Last_Name"));
+                    String studentNumber = String.valueOf(rs.getString("Student_Number"));
+                    String studentGradeLevel = String.valueOf(rs.getString("Grade_Level"));
+
+                    String studentTbData[] = {studentFirstName, studentLastName, studentNumber, studentGradeLevel};
+                    studentsTableModel.addRow(studentTbData);
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
+            }
+        }
+        else if("ICT 12 B".equals(selectedSection)) {
+            System.out.println("Hello B");
+            try {
+                String sqlQuery = "SELECT * FROM ICT_12B";
+                st = studentConn.createStatement();
+                rs = st.executeQuery(sqlQuery);
+
+                DefaultTableModel studentsTableModel = (DefaultTableModel)studentsCardStudentsTable.getModel();
+                studentsTableModel.setRowCount(0);
+
+                while (rs.next()) {
+                    String studentFirstName = String.valueOf(rs.getString("Student_First_Name"));
+                    String studentLastName = String.valueOf(rs.getString("Student_Last_Name"));
+                    String studentNumber = String.valueOf(rs.getString("Student_Number"));
+                    String studentGradeLevel = String.valueOf(rs.getString("Grade_Level"));
+
+                    String studentTbData[] = {studentFirstName, studentLastName, studentNumber, studentGradeLevel};
+                    studentsTableModel.addRow(studentTbData);
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
+            }
+        }
+        else if("ICT 12 C".equals(selectedSection)) {
+            System.out.println("Hello C");
+            try {
+                String sqlQuery = "SELECT * FROM ICT_12C";
+                st = studentConn.createStatement();
+                rs = st.executeQuery(sqlQuery);
+
+                DefaultTableModel studentsTableModel = (DefaultTableModel)studentsCardStudentsTable.getModel();
+                studentsTableModel.setRowCount(0);
+
+                while (rs.next()) {
+                    String studentFirstName = String.valueOf(rs.getString("Student_First_Name"));
+                    String studentLastName = String.valueOf(rs.getString("Student_Last_Name"));
+                    String studentNumber = String.valueOf(rs.getString("Student_Number"));
+                    String studentGradeLevel = String.valueOf(rs.getString("Grade_Level"));
+
+                    String studentTbData[] = {studentFirstName, studentLastName, studentNumber, studentGradeLevel};
+                    studentsTableModel.addRow(studentTbData);
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
+            }
+        }
+        else if ("ICT 12 D".equals(selectedSection)) {
+            System.out.println("Hello D");
+            try {
+                String sqlQuery = "SELECT * FROM ICT_12D";
+                st = studentConn.createStatement();
+                rs = st.executeQuery(sqlQuery);
+
+                DefaultTableModel studentsTableModel = (DefaultTableModel)studentsCardStudentsTable.getModel();
+                studentsTableModel.setRowCount(0);
+
+                while (rs.next()) {
+                    String studentFirstName = String.valueOf(rs.getString("Student_First_Name"));
+                    String studentLastName = String.valueOf(rs.getString("Student_Last_Name"));
+                    String studentNumber = String.valueOf(rs.getString("Student_Number"));
+                    String studentGradeLevel = String.valueOf(rs.getString("Grade_Level"));
+
+                    String studentTbData[] = {studentFirstName, studentLastName, studentNumber, studentGradeLevel};
+                    studentsTableModel.addRow(studentTbData);
+                }
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e);
+            }
+        }
+//        try {
+//            String sqlQuery = "SELECT * FROM ICT_12D";
+//            st = studentConn.createStatement();
+//            rs = st.executeQuery(sqlQuery);
+//
+//            DefaultTableModel studentsTableModel = (DefaultTableModel)studentsCardStudentsTable.getModel();
+//            studentsTableModel.setRowCount(0);
+//
+//            while (rs.next()) {
+//                String studentFirstName = String.valueOf(rs.getString("Student_First_Name"));
+//                String studentLastName = String.valueOf(rs.getString("Student_Last_Name"));
+//                String studentNumber = String.valueOf(rs.getString("Student_Number"));
+//                String studentGradeLevel = String.valueOf(rs.getString("Grade_Level"));
+//
+//                String studentTbData[] = {studentFirstName, studentLastName, studentNumber, studentGradeLevel};
+//                studentsTableModel.addRow(studentTbData);
+//            }
+//        } catch (Exception e) {
+//            JOptionPane.showMessageDialog(null, e);
+//        }
+    }//GEN-LAST:event_fetchNewDataButtonActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -342,11 +479,10 @@ public class newerMainPage extends javax.swing.JFrame {
     private javax.swing.JPanel cardPanel;
     private CustomizedElements.CustomizedButton dashboardButtonLabel;
     private javax.swing.JPanel dashboardPanel;
+    private javax.swing.JButton fetchNewDataButton;
     private javax.swing.JButton fetchStudentsDataButton;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSeparator jSeparator1;
@@ -354,7 +490,10 @@ public class newerMainPage extends javax.swing.JFrame {
     private javax.swing.JPanel navPanel;
     private CustomizedElements.CustomizedButton sectionsButtonLabel;
     private CustomizedElements.CustomizedButton studentsButtonLabel;
+    private javax.swing.JButton studentsCardSearchButton;
+    private javax.swing.JComboBox<String> studentsCardSectionsCombobox;
+    private javax.swing.JTextField studentsCardStudentsSearchField;
+    private javax.swing.JTable studentsCardStudentsTable;
     private javax.swing.JPanel studentsPanel;
-    private javax.swing.JTable studentsTable;
     // End of variables declaration//GEN-END:variables
 }
